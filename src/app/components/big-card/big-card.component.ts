3eb0f70a5ffde4d3db795a5f0a3dd8dc6a14bb83
lucaskaspary <lucaskaspary@hotmail.com>
2022-10-31 18:35:13 -0300

    Added dynamic entries to the components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = "";
+  @Input()
+  cardTitle:string = "Empty";
+  @Input()
+  cardDescription:string = "Empty";
+  @Input()
+  cardLink:string="#";
 
   constructor() { }
 
